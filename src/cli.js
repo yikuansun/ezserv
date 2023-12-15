d59991bc2bea8f31df64d3ef592696f134d5022b
Yikuan Sun <yikuansun2015@gmail.com>
2023-12-15 17:40:47 -0500

    Add port argument to CLI

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
 import LocalServer from "../lib/LocalServer.cjs";
+import minimist from "minimist";
 
-let serv = new LocalServer();
+let argv = minimist(process.argv.slice(2));
+let port = argv["port"] || argv["p"] || 0;
+let serv = new LocalServer(port);
 serv.dir = process.cwd();
